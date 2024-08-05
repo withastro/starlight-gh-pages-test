8e914ae2d5e4809fcceadb2aa194e5134ac16ae6
Chris Swithinbank <swithinbank@gmail.com>
2024-08-05 10:45:24 +0200

    Update `site` to match GH Pages custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://withastro.github.io',
+	site: 'https://astro.js.org',
 	base: 'starlight-gh-pages-test',
 	integrations: [
 		starlight({
